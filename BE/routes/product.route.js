003966f9f233a8d9e097ddb270b4e22a49f031b2
Vu Duc Viet <42991887+vietvd13@users.noreply.github.com>
2020-07-04 23:08:03 +0700

    Update product.route.js

--- BE/routes/product.route.js
+++ BE/routes/product.route.js
@@ -8,7 +8,7 @@ const product_controller = require('../controllers/product.controller');
 router.post('/create', product_controller.product_create);
 //Get products
 router.get('/get', product_controller.products_get);
-//Get product by name
+//Get product by id
 router.get('/get/:id', product_controller.product_get)
 //Update product by id
 router.put('/update/:id', product_controller.product_update);
